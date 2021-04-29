feace50d73f2779412d40e73a9080c908519b3c8
marcosvons <mvonstremayr@udesa.edu.ar>
2021-04-29 01:56:29 -0300

    mas estructura

--- proyecto_integrador/src/App.js
+++ proyecto_integrador/src/App.js
@@ -5,43 +5,58 @@ import Footer from "./components/Footer";
 
 
 //importo el componente que quiero de su archivo
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      users=[]
 
-  function App() {
-    return (
+    }
+  }
 
-      <React.Fragment>
-     
-      <div>
-         
-          <div id= "Header"> 
-              <Header/>
-          </div>
-      
-      
+  //funciones agregar numero tarjetas y borrar
+  borrarTarjeta(idTarjeta){
+    let resultado=this.state.users.filter((user)=>{
+      return user.id !== idTarjeta
+    })
+  }
+  //numero tarjeta con alert mensaje input
 
-          <div className="Cartas">
-          
-      {
-              Contacto.map(function(item, idx){
-                return(
-                    <div id='card' key={idx}>
-                        <Cartas Datos={item} />
-                    </div>
-                   )
-                })
-      }
-          </div>
-
-          <div id= "Footer"> 
-              <Footer/>
-          </div>
-
-      </div>
-
-</React.Fragment>
-    
-
-  );
+  componentDidMount(){
+    fetch("https://randomuser.me/api/?results=30")
+        .then(r => r.json())
+        .then((resultado)=>{
+            this.setState.users.push(resultado.results)
+        })
+        .catch((e)=>console.log(e))
 }
 
-export default App; 
+  render(){
+    return (
+            <div>           
+              <div id= "Header"> 
+                <Header/>
+              </div>
+                          
+              <div className="Cartas">         
+                {
+                  this.state.users.map((user)=>{
+                    return(
+                      <div className='card' key={idx}>
+                        <Cartas Datos={item} />
+                        <button onClick={this.borrarTarjeta.bind(this)}>Delete</button>
+                      </div>
+                    )
+                  }
+                }
+              </div>
+                
+              <div id= "Footer"> 
+                <Footer/>
+              </div>
+                
+            </div>
+    )
+  }
+
+}
--- proyecto_integrador/src/components/Cartas.js
+++ proyecto_integrador/src/components/Cartas.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import React, {Component} from 'react';
 
 
-function Cartas (props) {
+class Cartas extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
 
-    return(
-        <div id='card'>
-        <img src={props.Datos.image} alt="Foto del contacto"></img>
-        <h2>{props.Datos.apellido}</h2>
-        <h2>{props.Datos.nombre}</h2>
-        <h2>{props.Datos.mail}</h2>
-        <h2>{props.Datos.fechanacimiento}</h2>
-    </div>
-);
+        }
+    }
+
+    //FUNCIONES DE CAMBIO DE ESTADOS
+
+
+
+    render(){
+        return(
+            <div className='card-inner'>
+                <div className='frente-card'>
+                    <img src={props.Datos.image} alt="Foto del contacto"></img>
+                    <h2>{props.Datos.apellido}</h2>
+                    <h2>{props.Datos.nombre}</h2>
+                    <h2>{props.Datos.mail}</h2>
+                    <h2>{props.Datos.fechanacimiento}</h2>
+                </div>
+                <div className='detalle-card'>
+
+                </div>
+            </div>
+        )
+    };
 }
 //genero los componentes que quiero y los exporto
 
